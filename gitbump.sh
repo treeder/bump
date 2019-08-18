ea7d825ff2866882b7121be73a049b557f61bee5
Travis Reeder <treeder@gmail.com>
2019-08-17 17:33:17 -0700

    Add gitbump script

---
+++ gitbump.sh
@@ -0,0 +1,10 @@
+set -e
+
+oldv=$(git describe --match "v[0-9]*" --abbrev=0 HEAD)
+echo "oldv: $oldv"
+
+newv=$(docker run --rm -v "$PWD":/app treeder/bump --input "$oldv" patch)
+echo "newv: $newv"
+
+git tag -a "v$newv" -m "version $newv"
+git push --follow-tags
