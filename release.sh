9054a0afdddbd3df03afb40aa6f77178eeedece1
Travis Reeder <treeder@gmail.com>
2020-03-06 13:52:57 -0500

    clean up

--- release.sh
+++ release.sh
@@ -1,11 +1,13 @@
 set -ex
 
+# moved to GitHub Actions, so this file doesn't work anymore
+
 # SET BELOW TO YOUR DOCKER HUB USERNAME
 USERNAME=treeder
 IMAGE=bump
 
 # Get latest
-git pull
+# git pull
 
 # bump version
 # docker run --rm -v "$PWD":/app treeder/bump --filename VERSION "$(git log -1 --pretty=%B)"
@@ -19,13 +21,13 @@ git pull
 # git tag -a "v$version" -m "version $version"
 # git push --follow-tags
 
-# not using the file anymore, just git tags
-wget -O - https://raw.githubusercontent.com/treeder/bump/master/gitbump.sh | bash
-version=$(git tag --sort=-refname --list "v[0-9]*" | head -n 1)
-echo "new version $version"
+# # not using the file anymore, just git tags
+# wget -O - https://raw.githubusercontent.com/treeder/bump/master/gitbump.sh | bash
+# version=$(git tag --sort=-refname --list "v[0-9]*" | head -n 1)
+# echo "new version $version"
 
-# docker it
-echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
-docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
-docker push $USERNAME/$IMAGE:latest
-docker push $USERNAME/$IMAGE:$version
+# # docker it
+# echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
+# docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
+# docker push $USERNAME/$IMAGE:latest
+# docker push $USERNAME/$IMAGE:$version
