21177c26998c15e26dc40d3d3f731be53bd786ee
Travis Reeder <treeder@gmail.com>
2020-03-06 13:23:43 -0500

    Switching to version tags rather than version file

--- release.sh
+++ release.sh
@@ -8,20 +8,23 @@ IMAGE=bump
 git pull
 
 # bump version
-docker run --rm -v "$PWD":/app treeder/bump --filename VERSION "$(git log -1 --pretty=%B)"
-version=`cat VERSION`
-echo "version: $version"
-
-# ./build.sh
-
+# docker run --rm -v "$PWD":/app treeder/bump --filename VERSION "$(git log -1 --pretty=%B)"
+# version=`cat VERSION`
+# echo "version: $version"
 # tag it
-git add -A
-git commit -m "version $version"
-git tag -a "v$version" -m "version $version"
-git push --follow-tags
+# git config --global user.email "treeder+github@gmail.com"
+# git config --global user.name "Github Action"
+# git add -A
+# git commit -m "version $version"
+# git tag -a "v$version" -m "version $version"
+# git push --follow-tags
 
+# not using the file anymore, just git tags
+wget -O - https://raw.githubusercontent.com/treeder/bump/master/gitbump.sh | bash
+version=$(git tag --sort=-refname --list "v[0-9]*" | head -n 1)
+
+# docker it
+echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
 docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
-
-# push it
 docker push $USERNAME/$IMAGE:latest
 docker push $USERNAME/$IMAGE:$version
