a055838546bcbd50a15686c23a9137dbebc5182d
Travis Reeder <treeder@gmail.com>
2020-03-06 13:36:14 -0500

    wip

--- gitbump.sh
+++ gitbump.sh
@@ -3,6 +3,7 @@ set -e
 # git describe has issues with GitHub Actions: https://github.com/treeder/firetils/commit/160ef4560d8855c9c05f4cae207baeb71b7791f3/checks?check_suite_id=414542684
 # oldv=$(git describe --match "v[0-9]*" --abbrev=0 HEAD)
 # This new way seems to work better and avoids the issue above:
+git fetch --tags # checkout action does not get these
 oldv=$(git tag --sort=-refname --list "v[0-9]*" | head -n 1)
 echo "oldv: $oldv"
 
